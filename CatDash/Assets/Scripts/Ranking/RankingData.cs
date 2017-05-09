47d593e505c7287a0c6f2d282eec7f1e5ee26187
岡村 莉紗 <rokamura@geechs.com>
2017-05-09 16:11:26 +0900

    CatDash製作開始

---
+++ CatDash/Assets/Scripts/Ranking/RankingData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LitJson;
+
+public class RankingData : MonoBehaviour {
+
+
+	[SerializeField, HeaderAttribute ("名前")]
+	private Text nameTxt;
+	[SerializeField, HeaderAttribute ("順位")]
+	private Text rankTxt;
+	[SerializeField, HeaderAttribute ("スコア(pt)")]
+	private Text scoreTxt;
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	public void Init(JsonData rankData, int num){
+
+		var json = rankData ["ranking_data"];
+
+		this.nameTxt.text 	= (string)	json [num]["user_name"];
+		int score			= (int)		json [num]["score"];
+		this.scoreTxt.text 	= string.Format ("{0:#,0}Pt", score);
+		this.rankTxt.text 	= (num + 1).ToString ();
+
+	}
+}
