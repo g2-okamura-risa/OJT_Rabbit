a2a18b3d1c1c030f4309935f5c17a274e67df3f4
岡村 莉紗 <rokamura@geechs.com>
2017-05-09 17:57:22 +0900

    DOTween セットアップ 名前入力モーダル作成 タイトルコントローラー作成

--- CatDash/Assets/Scripts/Title/NameModalController.cs
+++ CatDash/Assets/Scripts/Title/NameModalController.cs
@@ -1,16 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System.Text;
 
 public class NameModalController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	[SerializeField, HeaderAttribute ("名前入力txt")]
+	private Text nameTxt;
+	[SerializeField, HeaderAttribute ("注意文言")]
+	private GameObject cautionObj;
+
+
+
+	private string userName;
+	private Encoding encJIS = Encoding.GetEncoding("Shift_JIS");
+
+	void Start(){
+
+		this.cautionObj.SetActive (false);
+
+		DOTween.Init (false, true, LogBehaviour.ErrorsOnly);
 	
-	// Update is called once per frame
-	void Update () {
-		
+
+		this.gameObject.transform.DOScale (new Vector3 (1.0f, 1.0f, 1.0f), 1.0f);
+	}
+
+
+	/// <summary>
+	/// 入力カーソルが外れたら実行
+	/// </summary>
+	public void NameInput(){
+
+		this.userName = nameTxt.text;
+
+	}
+
+
+
+	public void DecisionButton(){
+	
+		if (string.IsNullOrEmpty (this.userName)) {
+			cautionObj.SetActive (true);
+			return;
+		}
+
+		int num = encJIS.GetByteCount(this.userName);
+		bool isZenkaku = ( num == userName.Length * 2);
+
+		if (!isZenkaku) {
+			cautionObj.SetActive (true);
+			return;
+		}
+
+
+		//通信開始OK 遷移
+		cautionObj.SetActive (false);
 	}
 }
---
+++ CatDash/Assets/Scripts/Title/TitleController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TitleController : MonoBehaviour {
+
+	[SerializeField, HeaderAttribute ("モーダルobj")]
+	private GameObject modalObj;
+	[SerializeField, HeaderAttribute ("スタートボタン")]
+	private Button startButton;
+
+
+
+
+	private bool isNewUser = false;
+
+	// Use this for initialization
+	void Start () {
+		modalObj.SetActive (false);
+		//uuid設定
+
+		//なかったらボタンおしてモーダル表示
+		this.isNewUser = true;
+
+		//あったらそのままスタート
+
+
+	}
+	
+
+	public void ToGame(){
+
+		if (this.isNewUser) {
+			modalObj.SetActive (true);
+			startButton.enabled = false;
+		} else {
+
+			//ゲームへ遷移
+
+
+		}
+
+	
+	}
+}
