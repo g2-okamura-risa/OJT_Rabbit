7b46592bc4a7196e46b8a51eb7eb0ff43694e027
岡村 莉紗 <rokamura@geechs.com>
2017-05-11 12:17:11 +0900

    タイマー・距離実装 プレイヤー制御開始

---
+++ CatDash/Assets/Scripts/Game/Distance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Distance : MonoBehaviour {
+
+	[SerializeField, HeaderAttribute("距離テキスト")]
+	private Text distanceTxt;
+
+	private float si = 0.0f;
+	// Use this for initialization
+	void Start () {
+
+		distanceTxt.text = "0.0";
+
+	}
+		
+	public void SetDistance(float distance){
+
+		this.distanceTxt.text = distance.ToString ("F1");
+
+	}
+}
---
+++ CatDash/Assets/Scripts/Game/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour {
+
+	[SerializeField, HeaderAttribute("タイマーtxt")]
+	private Text timerTxt;
+
+	/// <summary>足ボタンがタップされたらtrue </summary>
+	public bool countStart = false;
+
+	private float time = 0;
+
+
+	// Use this for initialization
+	void Start () {
+
+		timerTxt.text = "0.00";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!countStart)
+			return;
+
+		//カウント開始
+		time += 1 * Time.deltaTime;
+		timerTxt.text = time.ToString ("F2");
+
+		
+	}
+}
