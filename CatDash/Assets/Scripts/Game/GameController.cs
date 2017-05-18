de7d1d38190e9ae5137b2b3788b27ed4304c955b
岡村 莉紗 <rokamura@geechs.com>
2017-05-18 16:53:45 +0900

    gameController修正 playerに処理分布

--- CatDash/Assets/Scripts/Game/GameController.cs
+++ CatDash/Assets/Scripts/Game/GameController.cs
@@ -30,25 +30,19 @@ public class GameController : MonoBehaviour {
 	[SerializeField, HeaderAttribute("画面全タップボタン")]
 	private GameObject screenBtnObj;
 
-	[SerializeField, HeaderAttribute("")]
+	[SerializeField, HeaderAttribute("セッション切れモーダルプレハブ")]
 	private GameObject limitOverObj;
 
 
 	private bool isTurnOver = false; //転んだらtrue; 2秒間ボタン停止
-	private int turnOver = 0;
 
-	private Vector3 pos;
-	private float btnStopCount = 0.0f;
-	private float dis = 0.0f;
-	private int leftCnt = 0;
-	private int rightCnt = 0;
 	public 	bool isGameOver = false;
 
 
-	private const float STOP_TIME = 2.0f; 
+	/*private const float STOP_TIME = 2.0f; 
 	private const int CONTINUITY_NUM = 2; 
 	private const float MOVING_DISTANCE = 0.5f;
-	private const float MAX_MOVING_DISTANCE = 100.0f;
+	private const float MAX_MOVING_DISTANCE = 100.0f;*/
 
 
 	// Use this for initialization
@@ -64,101 +58,66 @@ public class GameController : MonoBehaviour {
 	void Update () {
 
 		if (this.isGameOver) {
+
+			this.time.countStart = false;
+
+			//ゴールアニメーション
+			player.SetState( Player.PLAYER_STATE.JUMP );
+
+			//全画面タップON
+			screenBtnObj.SetActive ( true );
+
+			this.isGameOver = false;
+
 			return;
 		}
-
-		//転んだとき
-		if (isTurnOver) {
-			//ボタンoff
-			this.isFootBtn (false);
-
-			//カウント開始
-			btnStopCount += 1 * Time.deltaTime;
-
-			if (btnStopCount >= STOP_TIME) {				
-
-				turnOver++; 
-				btnStopCount = 0.0f;
-				isTurnOver = false;
-				this.isFootBtn (true);
-
-			}
-				
-		} else {
 			
-			if (Input.GetKeyDown (KeyCode.A)) {
-		
+
+
+		if ( !isTurnOver ) {
+			
+			if ( Input.GetKeyDown ( KeyCode.A ) ) {
+
 				this.LeftBtn ();		
-		
-			}
-			if (Input.GetKeyDown (KeyCode.D)) {
+
+			} else if ( Input.GetKeyDown ( KeyCode.D ) ) {
 
 				this.RightBtn ();		
 
 			}
 		}
-
-
-
-
-		if (this.dis >= MAX_MOVING_DISTANCE) {
-
-			this.isGameOver = true;
-			this.time.countStart = false;
-			//ゴールアニメーション
-			player.SetState(Player.PLAYER_STATE.JUMP);
-			this.isFootBtn (false);
-			//全画面タップON
-			screenBtnObj.SetActive (true);
-
-		}
-
-
+			
 	}
 
 
 
-	#region 足ボタン
+	#region ボタン
 
 	public void LeftBtn(){
 
-
-		this.leftCnt++;
-		this.rightCnt = 0;
-
-		if (this.leftCnt >= CONTINUITY_NUM) {
-			this.leftCnt = 0;
-			this.isTurnOver = true;
-			player.SetState(Player.PLAYER_STATE.TURNOVER);
-			return;
-		}
-
+		time.countStart = true;
 		player.SetState(Player.PLAYER_STATE.LEFT_WALK);
-
-		playerCalculate ();
-
 
 	}
 
 
 	public void RightBtn(){
 
-		this.rightCnt++;
-		this.leftCnt = 0;
-	
-		if (this.rightCnt >= CONTINUITY_NUM) {
-			this.rightCnt = 0;
-			this.isTurnOver = true;
-			player.SetState(Player.PLAYER_STATE.TURNOVER);
-			return;
-		}
+		time.countStart = true;
 
 		player.SetState(Player.PLAYER_STATE.RIGHT_WALK);
 
-		playerCalculate ();
 
 	}
 
+	public void isFootBtn(bool isActive){
+
+		this.footBtn [0].interactable = isActive;
+		this.footBtn [1].interactable = isActive;
+
+		// ボタンoff = ころんだ
+		this.isTurnOver = !isActive;
+	}
 
 	public void ScreenTap(){
 	
@@ -168,29 +127,8 @@ public class GameController : MonoBehaviour {
 	}
 
 
+
 	#endregion
-	private void playerCalculate(){
-
-		time.countStart = true;
-
-		this.dis += MOVING_DISTANCE;
-		this.distanceController.SetDistance (this.dis);
-		this.bgController.SetScroll ();
-
-
-
-	}
-
-	private void isFootBtn(bool isActive){
-
-		this.footBtn [0].interactable = isActive;
-		this.footBtn [1].interactable = isActive;
-
-	}
-
-
-
-
 
 	#region API
 
@@ -200,7 +138,8 @@ public class GameController : MonoBehaviour {
 		WWWForm w = new WWWForm();
 		w.AddField ("auth_token", Config.AUTH_TOKEN);
 		w.AddField ("goal_time", this.time.time.ToString());
-		w.AddField ("turnover_num", this.turnOver);
+		w.AddField ("turnover_num", player.turnOver);
+		Debug.Log ( "最終的に"+player.turnOver+"回転んだ" );
 
 		API api = new API ();
 		api.limitOverObj = this.limitOverObj;
@@ -217,4 +156,9 @@ public class GameController : MonoBehaviour {
 
 
 	#endregion
+
+
+
+	
+
 }
