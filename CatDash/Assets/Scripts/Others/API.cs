4836e66a3db22bb2efca56f1920bbff1c80108ae
岡村 莉紗 <rokamura@geechs.com>
2017-05-22 16:15:06 +0900

    FB反映 titleController,NameModalController修正

--- CatDash/Assets/Scripts/Others/API.cs
+++ CatDash/Assets/Scripts/Others/API.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 using DG.Tweening;
 using LitJson;
 
-public class API: MonoBehaviour{
+public class API : MonoBehaviour{
 
 	private Action< LitJson.JsonData > callback;
 	private TransitionController transition;
@@ -18,16 +18,14 @@ public class API: MonoBehaviour{
 
 	public IEnumerator Connect ( string path, WWWForm data, TransitionController transition, Action< JsonData > callback=null ) {
 		//TODO:トークンチェック追加
-		using(WWW www = new WWW( path, data )){
+		using ( WWW www = new WWW ( path, data ) ) {
 
-			Debug.Log(path);
-
-			while (!www.isDone) {
+			while ( !www.isDone ) {
 				Debug.Log ( "NowLoading" );
 				yield return www;
 			}
 
-			if(!string.IsNullOrEmpty(www.error)){
+			if ( !string.IsNullOrEmpty ( www.error ) ) {
 				Debug.LogError ( "www Error:" + www.error );
 				yield break;
 			}
@@ -35,74 +33,13 @@ public class API: MonoBehaviour{
 			Debug.Log ( www.text );
 			Debug.Log ( path );
 
-			this.callback 	= callback;
+			this.callback = callback;
 			this.transition = transition;
 			this.GetData ( www );
-
 		}
+
 	}
 
-
-
-	/// <summary>
-	/// データ受け取り用
-	/// </summary>
-	/// <returns>The get.</returns>
-	/// <param name="path">Path.</param>
-	/// <param name="callback">Callback.</param>
-	public IEnumerator ConnectGet ( string path, TransitionController transition, Action< JsonData > callback=null ) {
-
-		Debug.Log (path);
-
-		using(WWW www = new WWW(path)){
-
-			while (!www.isDone) {
-				Debug.Log ( "NowLoading" );
-				yield return www;
-			}
-
-			if(!string.IsNullOrEmpty(www.error)){
-				Debug.LogError ( "www Error:" + www.error );
-				yield break;
-			}
-
-			this.callback 	= callback;
-			this.transition = transition;
-			Debug.Log ( www.text );
-			this.GetData ( www );
-
-		}
-	}
-
-
-
-	/// <summary>
-	/// データ送信用
-	/// </summary>
-	/// <returns>The send.</returns>
-	/// <param name="path">Path.</param>
-	/// <param name="data">Data.</param>
-	/// <param name="callback">Callback.</param>
-	public IEnumerator ConnectSend ( string path, WWWForm data, Action callback = null ) {
-
-		//TODO:トークンチェック追加
-
-		using( WWW www = new WWW( path, data ) ){
-
-			while ( !www.isDone ) {
-				Debug.Log ( "NowLoading" );
-				yield return www;
-			}
-
-			if( !string.IsNullOrEmpty( www.error ) ){
-				Debug.LogError( "www Error:" + www.error );
-				yield break;
-			}
-
-			callback();
-
-		}
-	}
 
 	private void GetData( WWW w ){
 
---
+++ CatDash/Assets/Scripts/Title/LoginModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+public class LoginModel{
+
+	private string uuid;
+	private string name;
+
+
+	//イベント発行
+	public event EventHandler newUserDataHandler;
+	public event EventHandler transitionHandler;
+
+	public IEnumerator Loading(TransitionController transition){
+
+		uuid = PlayerPrefs.GetString ( Config.PREFS_KEY_UUID );
+
+		if ( uuid == "" ) {
+
+			newUserDataHandler ( this, EventArgs.Empty );
+			this.CreateUUID ();
+			yield break;
+		} 
+
+
+
+
+		//uuid送信しtokenもらう
+		WWWForm w = new WWWForm();
+		w.AddField ( "uuid", uuid );
+
+		if ( name != null ) {
+		
+			w.AddField ( "user_name", name );
+		
+		}
+
+		API api = new API ();
+	
+		yield return api.Connect ( Config.URL_LOGIN, w, transition, GetUserData );
+	
+	}
+
+
+	private void GetUserData( JsonData json ){
+
+		Config.AUTH_TOKEN 	= (string)	json["auth_token"];
+		Config.USER_ID 		= (int)		json ["user_id"];
+		transitionHandler ( this, EventArgs.Empty );
+	}
+		
+
+
+	private void CreateUUID(){
+
+		uuid = System.Guid.NewGuid ().ToString ();
+		PlayerPrefs.SetString (Config.PREFS_KEY_UUID, uuid);
+		PlayerPrefs.Save ();
+
+	}
+
+
+	public void SetUserName(string name){
+		this.name = name;
+	}
+
+
+
+
+
+
+
+
+}
+
+
+
+
